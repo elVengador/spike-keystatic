b590c231d4196764b79a405b6fb31cd6b8514b45
elVengador <dev.jimynicanor@gmail.com>
2026-07-20 12:48:58 -0500

    chore: first post

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod'
+import { glob } from 'astro/loaders';
+
+const posts = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdoc}', base: './src/content/posts' }),
+    schema: z.object({
+        title: z.string(),
+    }),
+});
+
+export const collections = { posts };
